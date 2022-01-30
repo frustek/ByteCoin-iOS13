6939116aa42a7bc22391a70a7cc00c9a279d0ea8
Adam Mroz <MrozAdam1992@gmail.com>
2022-01-30 14:46:44 +0100

    creating URL session

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -11,11 +11,22 @@ import Foundation
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    let apiKey = "F742A8B7-46C3-4BCF-8EE8-5771955204A6"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
     func getCoinPrice(for currency: String) {
-//        print(currency)
+        let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
+        
+        if let url = URL(string: urlString) {
+            
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url ) { (data, response, error) in
+                if error != nil {
+                    print(error)
+                }
+            }
+            
+        }
     }
 }
